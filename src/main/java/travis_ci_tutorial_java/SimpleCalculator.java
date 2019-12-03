f154b5b1f8fdf81243fe4d1c30155be8a1e8486e
Tommy <Tommy@192.168.1.6>
2019-12-02 20:23:05 -0500

    added operators

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,19 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int mult(int a, int b) {
+		return a*b;
+	}
+	
+	public int div(int a, int b) {
+		if(b==0) {
+			throw new IllegalArgumentException("Can't divide by 0");
+		}
+		return a/b;
+	}
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,18 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.subtract(5, 3), 2);
+	}
+	
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.mult(5, 2), 10);
+	}
+	
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.div(10, 5), 2);
+	}
 }
